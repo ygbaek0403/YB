3a2c94a0d1ca6909383498e0c4105aca422b9cb6
Young-Gwang Baek <ygbaek0403@gmail.com>
2017-09-26 17:49:48 -0400

    test

--- Google Drive/Ecllipse/workspace/Assignment01/src/chapter02/chapter02_08.java
+++ Google Drive/Ecllipse/workspace/Assignment01/src/chapter02/chapter02_08.java
@@ -8,6 +8,19 @@ public class chapter02_08 {
 		
 		Scanner input = new Scanner(System.in);
 		
+		long time = System.currentTimeMillis();
+		long remainder = time % (1000 * 60 * 60 * 24);
+		long hour = remainder / (1000 * 60 * 60);
+		long min = remainder % (1000 * 60 * 60) / (1000 * 60);
+		long sec = remainder / 1000 % 60;
+		long offset;
+	
+		System.out.print("Enter the time zone offset to GMT: ");
+		offset = input.nextLong();
+		
+		hour = (hour + offset) % 24;
+		
+		System.out.printf("%d : %d : %d", hour, min, sec);
 		
 	}
 }
